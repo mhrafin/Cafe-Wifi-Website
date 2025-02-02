7e6d4837bcf9a3cd9fbb1a767db3f2f68ef86ff8
raflinhom <110781737+mhrafin@users.noreply.github.com>
2025-02-02 10:10:37 +0600

    Be able to add cafe

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import os
 import secrets
 
-from flask import Flask, render_template
+from flask import Flask, redirect, render_template
+from flask_sqlalchemy import SQLAlchemy
 from flask_wtf import FlaskForm
+from sqlalchemy import Boolean, Float, Integer, String
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 from wtforms import (
     BooleanField,
     FloatField,
@@ -16,11 +19,42 @@ from wtforms.validators import URL, DataRequired
 app = Flask(__name__)
 
 app.config["SECRET_KEY"] = secrets.token_hex(16)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///project.db"
 
+
+# SQLAlchemy setup
+class Base(DeclarativeBase):
+    pass
+
+
+db = SQLAlchemy(model_class=Base)
+db.init_app(app)
+
+
+# Database Models
+class Cafe(db.Model):
+    id: Mapped[int] = mapped_column(
+        Integer, primary_key=True, autoincrement=True, nullable=False
+    )
+    name: Mapped[str] = mapped_column(String, nullable=False, unique=True)
+    short_description: Mapped[str] = mapped_column(String, nullable=True)
+    map_url: Mapped[str] = mapped_column(String, nullable=False)
+    img_url: Mapped[str] = mapped_column(String, nullable=False)
+    location: Mapped[str] = mapped_column(String(100), nullable=False)
+    has_sockets: Mapped[bool] = mapped_column(Boolean, nullable=False)
+    has_toilet: Mapped[bool] = mapped_column(Boolean, nullable=False)
+    has_wifi: Mapped[bool] = mapped_column(Boolean, nullable=False)
+    can_take_calls: Mapped[bool] = mapped_column(Boolean, nullable=False)
+    seats: Mapped[int] = mapped_column(Integer, nullable=False)
+    coffee_price: Mapped[float] = mapped_column(Float, nullable=False)
+
+with app.app_context():
+    db.create_all()
 
 # Forms
 class AddNewCafeForm(FlaskForm):
     name = StringField("Cafe Name", validators=[DataRequired()])
+    short_description = StringField("Short Description")
     map_url = URLField("Map Url", validators=[DataRequired(), URL()])
     img_url = URLField("Image Url", validators=[DataRequired(), URL()])
     location = StringField("Location", validators=[DataRequired()])
@@ -35,7 +69,8 @@ class AddNewCafeForm(FlaskForm):
 
 @app.route("/")
 def hello_world():
-    return render_template("home.html")
+    all_cafes = db.session.execute(db.select(Cafe)).scalars().all()
+    return render_template("home.html", cafes=all_cafes)
 
 
 @app.route("/add-cafe", methods=["GET", "POST"])
@@ -43,7 +78,23 @@ def add_cafe():
     form = AddNewCafeForm()
     print("Validate on submit:", form.validate_on_submit())
     if form.validate_on_submit():
-        print("Form Data:", form.data)
+        new_cafe =  Cafe()
+        new_cafe.name = form.data.get("name")
+        new_cafe.short_description = form.data.get("short_description")
+        new_cafe.map_url = form.data.get("map_url")
+        new_cafe.img_url = form.data.get("img_url")
+        new_cafe.location = form.data.get("location")
+        new_cafe.has_sockets = form.data.get("has_sockets")
+        new_cafe.has_toilet = form.data.get("has_toilet")
+        new_cafe.has_wifi = form.data.get("has_wifi")
+        new_cafe.can_take_calls = form.data.get("can_take_calls")
+        new_cafe.seats = form.data.get("seats")
+        new_cafe.coffee_price = form.data.get("coffee_price")
+
+        db.session.add(new_cafe)
+        db.session.commit()
+        # print("Form Data:", form.data)
+        return redirect("/")
     return render_template("add_cafe.html", form=form)
 
 
